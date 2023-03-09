5e53752c870e3ffe1b4e1a66c0dc1a1d78b9aebd
bashlogs <mayurkhadde49@gmail.com>
2023-03-09 20:21:18 +0530

    Done

--- .config/openbox/temp/polybar/launch.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
-killall -q polybar
-#polybar bar1 2>&1 & disown
-polybar -q bar1 -c "$HOME/.config/openbox/polybar/config.ini" &
--- .config/ranger/plugins/__init__.py
+++
@@ -1,6 +0,0 @@
-####################################################################################
-#     >>  Maintained by team Vitunix (https://github.com/vitunix)  <<              #
-#             >> A Simple and unique Arch based distro <<                          #
-#             -----> Mail us at (vitunix@proton.me) <-----                         #
-#     ----> configured by heapbytes (https://github.com/heapbytes) <-----          #
-####################################################################################
